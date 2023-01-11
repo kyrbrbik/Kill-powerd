8a704294d14637fc1bd8d86c6c41fd8935202b58
kyrbrbik <103336573+kyrbrbik@users.noreply.github.com>
2023-01-11 15:18:32 +0100

    top.temp now saved to /tmp

--- kill_powerd.sh
+++ kill_powerd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-top -b -w 512 -n 1 > top.temp
-usage=$(grep nvidia-powerd /root/top.temp | awk '{print $9}')
-rm top.temp
+top -b -w 512 -n 1 > /tmp/top.temp
+usage=$(grep nvidia-powerd /tmp/top.temp | awk '{print $9}')
+rm /tmp/top.temp
 usage_rounded=$(printf %.0f $usage)
 if [[ "$usage_rounded" -gt "80" ]] 
 then
