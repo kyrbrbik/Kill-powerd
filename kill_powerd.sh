901a7d00783f1932e78ef17d88d086fdc10defba
kyrbrbik <103336573+kyrbrbik@users.noreply.github.com>
2023-01-11 19:48:47 +0100

    fixed log location and removed temp file for top

--- kill_powerd.sh
+++ kill_powerd.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-top -b -w 512 -n 1 > /tmp/top.temp
-usage=$(grep nvidia-powerd /tmp/top.temp | awk '{print $9}')
-rm /tmp/top.temp
+usage=$(echo "$(top -b -w 512 -n 1)" | grep nvidia-powerd | awk '{print $9}')
 usage_rounded=$(printf %.0f $usage)
 if [[ "$usage_rounded" -gt "80" ]] 
 then
 	systemctl restart nvidia-powerd.service
 	#Uncomment the next line if you want to log the restarts
-	#echo "$(date +%F-%R) powerd was restarted" >> /root/powerd.log
+	#echo "$(date +%F-%R) powerd was restarted" >> /var/log/powerd.log
 fi
